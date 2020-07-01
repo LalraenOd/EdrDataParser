a6ab3a1857e7ce49c22fd20e9ecc99f33efcb7d3
Владимир Миненко <minenko.od@gmail.com>
2020-07-01 12:06:05 +0300

    COSMETIC AND STARTED ORGANISATION CLASS

--- EdrDataParser/EdrDataParserClass.cs
+++ EdrDataParser/EdrDataParserClass.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace EdrDataParser
 {
     public class EdrDataParserClass
     {
+        /// <summary>
+        /// Gets data from open API edr.data-gov-ua.org/api
+        /// </summary>
+        /// <param name="listEdrpou">Input list of EDRPOU codes to get info about</param>
+        /// <returns>List of strings with basic info like: FullName, Occupation and Status.</returns>
         public static List<string> EdrpouDetailParser(List<string> listEdrpou)
         {
             List<string> resultList = new List<string>();
@@ -18,8 +20,8 @@ namespace EdrDataParser
                 client.Encoding = Encoding.UTF8;
                 for (int listItemCount = 0; listItemCount < listEdrpou.Count - 1; listItemCount++)
                 {
-                    var edrpou = listEdrpou[listItemCount];
-                    var linkEdrpou = "http://edr.data-gov-ua.org/api/companies?where={\"edrpou\":{\"contains\":\"" + edrpou + "\"}}";
+                    string edrpou = listEdrpou[listItemCount];
+                    string linkEdrpou = "http://edr.data-gov-ua.org/api/companies?where={\"edrpou\":{\"contains\":\"" + edrpou + "\"}}";
                     string pageEdrpou = client.DownloadString(linkEdrpou);
                     Regex regexOfName = new Regex("\"officialName\":(?<result>.+)\",\"address");
                     Regex regexOccupation = new Regex("\"occupation\":\"(?<result>.+)\",\"status");
@@ -27,10 +29,12 @@ namespace EdrDataParser
                     Match matchName = regexOfName.Match(pageEdrpou);
                     Match matchOcc = regexOccupation.Match(pageEdrpou);
                     Match matchStatus = regexStatus.Match(pageEdrpou);
-                    string resultName = matchName.Groups[1].Value;
-                    string resultOcc = matchOcc.Groups[1].Value;
-                    string resultStatus = matchStatus.Groups[1].Value;
-                    string result = (listItemCount + 1) + "\t" + edrpou + "\t" + resultName + "\t" + resultOcc + "\t" + resultStatus;
+                    string result = 
+                        (listItemCount + 1) + "\t" +
+                        edrpou + "\t" +
+                        matchName.Groups[1].Value.ToString() + "\t" +
+                        matchOcc.Groups[1].Value.ToString() + "\t" +
+                        matchStatus.Groups[1].Value.ToString();
                     resultList.Add(result);
                 }
                 return resultList;
---
+++ EdrDataParser/Organisation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdrDataParser
+{
+    class Organisation
+    {
+        public string EdrpouCode { get; set; }
+        public string OfficialName { get; set; }
+        public string Occupation { get; set; }
+        public string Status { get; set; }
+        public string Head { get; set; }
+
+
+
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
